838bda4370a2e981d30e88496e97664039897440
xuyanMax <418780414@qq.com>
2016-05-04 15:56:41 +0800

    timeline<-HomeController->Auth::check(); timeline.index

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,12 +3,16 @@
 namespace SocialNetworkApp\Http\Controllers;
 
 
-
+use Auth;
 class HomeController extends Controller {
     
     
     public function index(){
         
+        if (Auth::check()) {
+            
+            return view('timeline.index');    
+        }
         return view('home');
     }
 }
